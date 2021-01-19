d0ebb15b8b035f7b351c5791961bf6ae89127ba9
Mike <>
2021-01-19 14:00:29 +0100

    Implement task

---
+++ src/test/java/com/mdziedzic/interview/samplerestservice/restcontroller/TriggerControllerTest.java
@@ -0,0 +1,31 @@
+package com.mdziedzic.interview.samplerestservice.restcontroller;
+
+import com.mdziedzic.interview.samplerestservice.jms.JmsPublisher;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TriggerControllerTest {
+
+    @InjectMocks
+    TriggerController cut;
+    @Mock
+    private JmsPublisher jmsPublisher;
+
+    @Test
+    void whenCallTriggerEndpointShouldPublishMessage() {
+        //given
+        doNothing().when(jmsPublisher).publish(anyString());
+        //when
+        cut.sendTriggerMessage();
+        //then
+        verify(jmsPublisher).publish(anyString());
+    }
+}
